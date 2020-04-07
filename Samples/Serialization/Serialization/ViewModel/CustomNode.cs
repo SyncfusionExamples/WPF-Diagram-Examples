524dfdbc0e3f3397eb1289960d5bb882d0e698e4
Karkuvelrajan <karkuvelrajan.shanmugavel@syncfusion.com>
2020-04-07 20:00:49 +0530

    WPF-51795 Serialization sample is published

---
+++ Samples/Serialization/Serialization/ViewModel/CustomNode.cs
@@ -0,0 +1,98 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Serialization_WPF.ViewModel
+{
+    // CustomNode class for node
+    // No need to mention DataContract as this class is derived from SfDiagram's ViewModel
+    public class CustomNode : NodeViewModel
+    {
+        private string _mcustomshape;
+        private string _mcustomstyle;
+        private NodeContent _mcustomcontent;
+
+        [DataMember]
+        public string CustomShape
+        {
+            get
+            {
+                return _mcustomshape;
+            }
+            set
+            {
+                if (_mcustomshape != value)
+                {
+                    _mcustomshape = value;
+                    Shape = Application.Current.Resources[_mcustomshape];
+                    OnPropertyChanged("CustomShape");
+                }
+            }
+        }
+
+        [DataMember]
+        public string CustomStyle
+        {
+            get
+            {
+                return _mcustomstyle;
+            }
+            set
+            {
+                if (_mcustomstyle != value)
+                {
+                    _mcustomstyle = value;
+                    ShapeStyle = App.Current.Resources[_mcustomstyle] as Style;
+                    OnPropertyChanged("CustomStyle");
+                }
+            }
+        }
+
+        [DataMember]
+        public NodeContent CustomContent
+        {
+            get
+            {
+                return _mcustomcontent;
+            }
+            set
+            {
+                if (_mcustomcontent != value)
+                {
+                    _mcustomcontent = value;
+                    OnPropertyChanged("CustomContent");
+                }
+            }
+        }
+    }
+
+    // Custom Class for Content
+    // Need to mention the DataContract as this class is not derived from any of our Viewmodel.
+
+    [DataContract]
+    public class NodeContent
+    {
+        private string _mcontent;
+
+        [DataMember]
+        public string Content
+        {
+            get
+            {
+                return _mcontent;
+            }
+            set
+            {
+                if (_mcontent != value)
+                {
+                    _mcontent = value;
+                }
+            }
+        }
+    }
+}
---
+++ Samples/Serialization/Serialization/ViewModel/DiagramVM.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using Serialization_WPF.Utility;
+using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Serialization_WPF.ViewModel
+{
+    public class DiagramVM : DiagramViewModel
+    {
+        private ICommand _SaveCommand;
+        private ICommand _LoadCommand;
+
+        public ICommand SaveCommand
+        {
+            get { return _SaveCommand; }
+            set { _SaveCommand = value; }
+        }
+
+        public ICommand LoadCommand
+        {
+            get { return _LoadCommand; }
+            set { _LoadCommand = value; }
+        }
+
+        public DiagramVM()
+        {
+            // Initialize ItemAdded command to add the content for loaded node
+            ItemAddedCommand = new Command(OnItemAdded);
+
+            LoadCommand = new Command(OnLoad);
+            SaveCommand = new Command(OnSave);
+
+            // Need to add new class which are not derived from our ViewModel classess
+            // Need to add new types
+
+            KnownTypes = () => new List<Type>()
+            {
+                typeof(NodeContent)
+            };
+
+            CustomNode node1 = new CustomNode()
+            {
+                OffsetX = 150,
+                OffsetY = 150,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                ShapeStyle = App.Current.Resources["NodeStyle"] as Style,
+                Shape = App.Current.Resources["Rectangle"],
+                CustomShape = "Rectangle",
+                CustomContent = new NodeContent() { Content = "Node1" },
+                CustomStyle = "NodeStyle",
+                Content = new NodeContent() { Content = "Node1" },
+            };
+
+            (Nodes as NodeCollection).Add(node1);
+
+
+            CustomNode node2 = new CustomNode()
+            {
+                OffsetX = 300,
+                OffsetY = 300,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                ShapeStyle = App.Current.Resources["NodeStyle"] as Style,
+                Shape = App.Current.Resources["Ellipse"],
+                CustomShape = "Ellipse",
+                CustomContent = new NodeContent() { Content = "Node2" },
+                CustomStyle = "NodeStyle",
+                Content = new NodeContent() { Content = "Node2" },
+            };
+
+            (Nodes as NodeCollection).Add(node2);
+
+            ConnectorViewModel Con = new ConnectorViewModel()
+            {
+                SourceNode = node1,
+                TargetNode = node2,
+            };
+
+            (Connectors as ConnectorCollection).Add(Con);
+
+        }
+
+        private void OnSave(object obj)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Save XAML";
+            dialog.Filter = "XAML File (*.xaml)|*.xaml";
+            if (dialog.ShowDialog() == true)
+            {
+                using (Stream s = File.Open(dialog.FileName, FileMode.OpenOrCreate))
+                {
+                    (Info as IGraphInfo).Save(s);
+                }
+            }
+        }
+
+        private void OnLoad(object obj)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                using (Stream myStream = dialog.OpenFile())
+                {
+                    (Info as IGraphInfo).Load(myStream);
+                }
+            }
+        }
+
+        private void OnItemAdded(object obj)
+        {
+            var args = obj as ItemAddedEventArgs;
+            if (args.Item is CustomNode && args.ItemSource == ItemSource.Load)
+            {
+                (args.Item as CustomNode).Content = (args.Item as CustomNode).CustomContent;
+            }
+        }
+    }
+}
