3097342c377438d8a0b3c55daad41009639533ac
Ranjitha_amirthalingam <ranjitha.amirthalingam@syncfusion.com>
2020-04-09 15:32:33 +0530

    WPF-51798 Published interaction samples and committed.

---
+++ Samples/Interaction/KeyboardShortcuts-sample/KeyboardShortcut/ViewModel/CustomNode.cs
@@ -0,0 +1,58 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardShortcut.ViewModel
+{
+    public class CustomNode : NodeViewModel
+    {
+        private string _Id;
+
+        private string _ParentId;
+
+        [DataMember]
+        public string Id
+        {
+            get
+            {
+                return _Id;
+            }
+            set
+            {
+                if (_Id != value)
+                {
+                    _Id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+        [DataMember]
+        public string ParentId
+        {
+            get
+            {
+                return _ParentId;
+            }
+            set
+            {
+                if (_ParentId != value)
+                {
+                    _ParentId = value;
+                    OnPropertyChanged("ParentId");
+                }
+            }
+        }
+
+        protected override void OnPropertyChanged(string name)
+        {
+            base.OnPropertyChanged(name);
+        }
+    }
+}
+
+
