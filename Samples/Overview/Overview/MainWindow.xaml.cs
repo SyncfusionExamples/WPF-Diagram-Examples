18e0cdc7c30da037b6181382f2d7fc7bfeeaedc5
Ranjitha_amirthalingam <ranjitha.amirthalingam@syncfusion.com>
2020-04-13 11:54:43 +0530

    WPF-51798 published overview sample and committed.

---
+++ Samples/Overview/Overview/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.Windows.Shared;
+using System.Windows;
+using System.Windows.Media;
+
+namespace UserInteraction_Overview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
