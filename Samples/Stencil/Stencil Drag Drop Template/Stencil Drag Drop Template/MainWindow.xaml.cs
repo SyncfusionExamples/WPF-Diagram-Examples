a3da628c7d07a2f57bda581a2abdb48086a29697
Ranjitha_amirthalingam <ranjitha.amirthalingam@syncfusion.com>
2020-04-14 11:01:32 +0530

    WPF-49128 Preserving data template sample published in github.

---
+++ Samples/Stencil/Stencil Drag Drop Template/Stencil Drag Drop Template/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stencil_Drag_Drop_Template
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }      
+    }
+
+  
+}
