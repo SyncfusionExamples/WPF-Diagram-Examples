159af92fd55e53e9c44fb941960c828d622c74df
Ranjitha_amirthalingam <ranjitha.amirthalingam@syncfusion.com>
2020-04-14 14:03:07 +0530

    WPF-49128 Published SymbolFilters Sample.

---
+++ Samples/Stencil/SymbolFilters-sample/SymbolFilters/stencil/ViewModel/StencilVM.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.UI.Xaml.Diagram.Stencil;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace stencil.ViewModel
+{
+    public class StencilVM : INotifyPropertyChanged
+    {
+        public StencilVM()
+        {
+            Symbolfilters = new SymbolFilters();
+
+            SymbolFilterProvider all = new SymbolFilterProvider { Content = "All", SymbolFilter = Filter };
+            SymbolFilterProvider Node = new SymbolFilterProvider { Content = "Nodes", SymbolFilter = Filter };
+            SymbolFilterProvider Con = new SymbolFilterProvider { Content = "Connector", SymbolFilter = Filter };
+
+            this.Symbolfilters.Add(all);
+            this.Symbolfilters.Add(Node);
+            this.Symbolfilters.Add(Con);
+            this.Selectedfilter = Symbolfilters[0];
+        }
+
+        // Define filtering of Symbols
+        private bool Filter(SymbolFilterProvider sender, object symbol)
+        {
+            if (symbol is NodeViewModel)
+            {
+                if (sender.Content.ToString() == (symbol as NodeViewModel).Key.ToString())
+                    return true;
+            }
+
+            if (symbol is ConnectorViewModel)
+            {
+                if (sender.Content.ToString() == (symbol as ConnectorViewModel).Key.ToString())
+                    return true;
+            }
+            if (sender.Content.ToString() == "All")
+            {
+                return true;
+            }
+            return false;
+        }
+        public ObservableCollection<SymbolFilterProvider> Symbolfilters { get; set; } 
+
+        public SymbolFilterProvider Selectedfilter { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
