472dda3508c3a5fe06a6a9065f3b22ce1643f1bf
deepathiruppathy01 <deepa.thiruppathy@syncfusion.com>
2020-04-17 17:54:53 +0530

    WPF-46107 How to override the default cursors while interaction in WPF Diagram(SfDiagram)?
    WPF-46107
    How to override the default cursors while interaction in WPF Diagram(SfDiagram)?

---
+++ Samples/Tools/SetCursorSample/SetCursorSample/View/CustomClass.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SetCursorKBSample
+{
+    /// <summary>
+    /// Create custom class of SfDiagram to override the cursor
+    /// </summary>
+    public class CustomClass : SfDiagram
+    {
+        //Override method to customize the default cursors of diagram objects
+        protected override void SetCursor(SetCursorArgs args)
+        {
+            if (args.Source is INode)
+            {
+                args.Cursor = Cursors.No;
+            }
+            else if (args.Source is IConnector)
+            {
+                args.Cursor = Cursors.Hand;
+            }
+            else if (args.Source is IPort)
+            {
+                args.Cursor = Cursors.SizeAll;
+            }
+            else
+            {
+                base.SetCursor(args);
+            }
+        }
+
+    }
+}
---
+++ Samples/Tools/SetCursorSample/SetCursorSample/ViewModel/CustomViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetCursorKBSample
+{
+    public class CustomViewModel : DiagramViewModel
+    {
+        public CustomViewModel()
+        {
+            //Set the port visibility as visible
+            this.PortVisibility = PortVisibility.Visible;
+
+            //Initialize the nodes and connectors collection
+            this.Nodes = new ObservableCollection<NodeViewModel>();
+            this.Connectors = new ObservableCollection<ConnectorViewModel>();
+
+            //Create the source node
+            NodeViewModel sourceNode = new NodeViewModel()
+            {
+                OffsetX = 200,
+                OffsetY = 200,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Ports = new PortCollection()
+                {
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 1,
+                        NodeOffsetY = 1,
+                    }
+                }
+            };
+
+            //Create the source node
+            NodeViewModel targetNode = new NodeViewModel()
+            {
+                OffsetX = 400,
+                OffsetY = 400,
+                UnitHeight = 100,
+                UnitWidth = 100,
+            };
+
+            //Create the connector.
+            ConnectorViewModel connector = new ConnectorViewModel()
+            {
+                SourceNode = sourceNode,
+                TargetNode = targetNode,
+            };
+
+            //Add the nodes into nodes collection.
+            (this.Nodes as ObservableCollection<NodeViewModel>).Add(sourceNode);
+            (this.Nodes as ObservableCollection<NodeViewModel>).Add(targetNode);
+
+            //Add the connectors into connectors collection.
+            (this.Connectors as ObservableCollection<ConnectorViewModel>).Add(connector);
+        }
+    }
+}
