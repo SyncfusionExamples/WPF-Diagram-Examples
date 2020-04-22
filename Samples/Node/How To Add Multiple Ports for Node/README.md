3e962259067d372386af947afa093bb65522a873
deepathiruppathy01 <deepa.thiruppathy@syncfusion.com>
2020-04-22 11:23:19 +0530

    WPF-41730 How to add multiple ports for Node in WPF Diagram(SfDiagram)?
    WPF-41730
    How to add multiple ports for Node in WPF Diagram(SfDiagram)?

---
+++ Samples/Node/How To Add Multiple Ports for Node/README.md
@@ -0,0 +1,15 @@
+# Sample for Adding multiple ports to Node
+
+This repository contains sample which shows how to add multiple ports to the Node in SfDiagram.
+
+UG Link: https://help.syncfusion.com/wpf/diagram/port/port#node-port 
+
+## Project pre-requisites
+
+To run this application, you need to have the below two in your system
+
+* [Visual Studio 2019](https://www.visualstudio.com/wpf-vs)
+* [Syncfusion.SfDiagram.WPF](https://www.nuget.org/packages/Syncfusion.SfDiagram.WPF/) nuget package. To install the package using NuGet Package Manager, refer this [link](https://docs.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-in-visual-studio#nuget-package-manager).
+
+## Deploying and running the sample
+* To debug the sample and then run it, press F5 or select Debug > Start Debugging. To run the sample without debugging, press Ctrl+F5 or selectDebug > Start Without Debugging.
---
+++ Samples/Node/How To Add Multiple Ports for Node/Sample/ViewModel/PortViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleNodePorts
+{
+    public class PortViewModel:DiagramViewModel
+    {
+        public PortViewModel()
+        {
+            //Initialize the node collection
+            this.Nodes = new ObservableCollection<NodeViewModel>();
+
+            //Enable the port visiblity.
+            this.PortVisibility = PortVisibility.Visible;
+
+            //Create the node
+            NodeViewModel node = new NodeViewModel()
+            {
+                UnitHeight = 100,
+                UnitWidth = 100,
+                OffsetX = 200,
+                OffsetY = 200,
+                //Create the ports collection
+                Ports = new PortCollection()
+                {
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 0,
+                        NodeOffsetY = 0,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 0.5,
+                        NodeOffsetY = 0,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 1,
+                        NodeOffsetY = 0,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 0,
+                        NodeOffsetY = 0.5,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 1,
+                        NodeOffsetY = 0.5,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 0,
+                        NodeOffsetY = 1,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 0.5,
+                        NodeOffsetY = 01,
+                    },
+                    new NodePortViewModel()
+                    {
+                        NodeOffsetX = 1,
+                        NodeOffsetY = 1,
+                    },
+
+                }
+            };
+
+            //Adding node into node collection.
+            (this.Nodes as ObservableCollection<NodeViewModel>).Add(node);
+        }
+    }
+}
