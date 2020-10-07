d2bafac6eb134ef463f1fee616fac0608144c987
KeerthivasanR <keerthivasan.ramamoorthy@syncfusion.com>
2020-10-07 19:11:43 +0530

    WPF-53809-Added BpmnEditor sample.

---
+++ Samples/BPMNEditor/Sample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.SfSkinManager;
+using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BPMNEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Diagram.Loaded += Diagram_Loaded;
+        }
+
+        private void Diagram_Loaded(object sender, RoutedEventArgs e)
+        {
+            (Diagram.Info as IGraphInfo).Commands.FitToPage.Execute(null);
+        }
+    }
+}
