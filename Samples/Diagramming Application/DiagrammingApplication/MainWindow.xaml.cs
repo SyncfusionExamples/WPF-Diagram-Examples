eea6516f6f3432f352d3b21676bfe28b240e9548
sarathkumarv <sarathkumarv@syncfusion.com>
2021-01-12 19:24:44 +0530

    Added sample for diagramming application

---
+++ Samples/Diagramming Application/DiagrammingApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Syncfusion.SfSkinManager;
+using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.Windows.Tools.Controls;
+
+namespace DiagrammingApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        private bool first = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            (diagramcontrol.Info as IGraphInfo).ViewPortChangedEvent += FlowDiagram_ViewPortChangedEvent;
+            SfSkinManager.SetTheme(this, new Theme() { ThemeName = "Office2019Colorful" });
+        }
+
+        private void FlowDiagram_ViewPortChangedEvent(object sender, ChangeEventArgs<object, ScrollChanged> args)
+        {
+            if (diagramcontrol.Info != null && (args.Item as SfDiagram).IsLoaded == true && first && args.NewValue.ContentBounds != args.OldValue.ContentBounds)
+            {
+                (diagramcontrol.Info as IGraphInfo).BringIntoCenter(args.NewValue.ContentBounds);
+                first = false;
+            }
+        }
+    }
+}
---
+++ Samples/Diagramming Application/DiagrammingApplication/ViewModel/CustomDiagramViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.UI.Xaml.Diagram.Controls;
+using Syncfusion.UI.Xaml.Diagram.Stencil;
+
+namespace DiagrammingApplication.ViewModel
+{
+    public class CustomDiagramViewModel : DiagramViewModel
+    {
+        public CustomDiagramViewModel()
+        {
+            Symbolfilters = new SymbolFilters();
+
+            SymbolFilterProvider basicshapes = new SymbolFilterProvider { Content = "Basic Shapes", SymbolFilter = Filter };
+            SymbolFilterProvider flowshapes = new SymbolFilterProvider { Content = "Flow Shapes", SymbolFilter = Filter };
+            SymbolFilterProvider dataflowshapes = new SymbolFilterProvider { Content = "DataFlow Shapes", SymbolFilter = Filter };
+            SymbolFilterProvider arrowshapes = new SymbolFilterProvider { Content = "Arrow Shapes", SymbolFilter = Filter };
+
+            this.Symbolfilters.Add(basicshapes);
+            this.Symbolfilters.Add(flowshapes);
+            this.symbolfilters.Add(dataflowshapes);
+            this.Symbolfilters.Add(arrowshapes);
+            this.Selectedfilter = Symbolfilters[0];
+        }
+
+        // Define filtering of Symbols
+        private bool Filter(SymbolFilterProvider sender, object symbol)
+        {
+            if (sender.Content.ToString() == (symbol as NodeViewModel).Key.ToString())
+            {
+                return true;
+            }
+            if (sender.Content.ToString() == (symbol as NodeViewModel).Key.ToString())
+            {
+                return true;
+            }
+            if (sender.Content.ToString() == (symbol as NodeViewModel).Key.ToString())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private SymbolFilters symbolfilters;
+
+        public SymbolFilters Symbolfilters
+        {
+            get
+            {
+                return symbolfilters;
+            }
+            set
+            {
+                symbolfilters = value;
+                OnPropertyChanged("Symbolfilters");
+            }
+        }
+
+        private SymbolFilterProvider selectedfilter;
+
+        public SymbolFilterProvider Selectedfilter
+        {
+            get
+            {
+                return selectedfilter;
+            }
+            set
+            {
+                selectedfilter = value;
+                OnPropertyChanged("Selectedfilter");
+            }
+        }
+    }
+}
