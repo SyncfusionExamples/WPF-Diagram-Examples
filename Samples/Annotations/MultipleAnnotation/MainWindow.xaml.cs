df37ee6283d4d993d8fd339ce4b9f39364bf683b
deepathiruppathy01 <deepa.thiruppathy@syncfusion.com>
2021-06-02 16:29:23 +0530

    Added multiple annotation sample
    Added multiple annotation sample

--- Samples/Annotations/MultipleAnnotation/MainWindow.xaml.cs
+++ Samples/Annotations/MultipleAnnotation/MainWindow.xaml.cs
@@ -25,48 +25,7 @@ namespace MultipleAnnotation
         public MainWindow()
         {
             InitializeComponent();
-            diagram.PageSettings.PageWidth = 1000;
-            diagram.PageSettings.PageHeight = 1000;
-            //Define the AnnotationCollection
-            ObservableCollection<AnnotationEditorViewModel> annotations = new ObservableCollection<AnnotationEditorViewModel>();
-            //Define Annotation
-            AnnotationEditorViewModel annotation1 = AddAnnotation("Name", this.Resources["viewtemplate"] as DataTemplate, new Point(0.2, 0.3));
-            AnnotationEditorViewModel annotation2 = AddAnnotation(":", this.Resources["viewtemplate"] as DataTemplate, new Point(0.5, 0.3));
-            AnnotationEditorViewModel annotation3 = AddAnnotation("John", this.Resources["viewtemplate"] as DataTemplate, new Point(0.7, 0.3));
-            AnnotationEditorViewModel annotation4 = AddAnnotation("DOB", this.Resources["viewtemplate"] as DataTemplate, new Point(0.2, 0.6));
-            AnnotationEditorViewModel annotation5 = AddAnnotation(":", this.Resources["viewtemplate"] as DataTemplate, new Point(0.5, 0.6));
-            AnnotationEditorViewModel annotation6 = AddAnnotation("3/3/95", this.Resources["viewtemplate"] as DataTemplate, new Point(0.7, 0.6));
-            //Adding Annotation to Collection
-            annotations.Add(annotation1);
-            annotations.Add(annotation2);
-            annotations.Add(annotation3);
-            annotations.Add(annotation4);
-            annotations.Add(annotation5);
-            annotations.Add(annotation6);
-            //Define the NodeCollection
-            diagram.Nodes = new NodeCollection();
-            //Define the Node
-            NodeViewModel nodes = new NodeViewModel()
-            {
-                UnitHeight = 65,
-                UnitWidth = 100,
-                OffsetX = 100,
-                OffsetY = 100,
-                Shape=App.Current.Resources["Terminator"],
-               Annotations=annotations,
-            };
-            //Adding Node to Collection
-            (diagram.Nodes as NodeCollection).Add(nodes);
-
         }
-       //Method for Annotation
-        private AnnotationEditorViewModel AddAnnotation(string content,DataTemplate Template,Point offset)
-        {
-            AnnotationEditorViewModel text = new AnnotationEditorViewModel();
-            text.Content = content;
-            text.ViewTemplate = Template;
-            text.Offset = offset;
-            return text;
-        }
+      
     }
 }
---
+++ Samples/Annotations/MultipleAnnotation/ViewModel/MultipleAnnotationVM.cs
@@ -0,0 +1,87 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.UI.Xaml.Diagram.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultipleAnnotation
+{
+    public class MultipleAnnotationVM : DiagramViewModel
+    {
+
+        ResourceDictionary resourceDictionary = new ResourceDictionary()
+        {
+            Source = new Uri(@"/Syncfusion.SfDiagram.Wpf;component/Resources/BasicShapes.xaml", UriKind.RelativeOrAbsolute)
+        };
+
+        public MultipleAnnotationVM()
+        {
+            //Define the NodeCollection
+            this.Nodes = new NodeCollection();
+
+            //Define the connectors collection
+            this.Connectors = new ConnectorCollection();
+
+            NodeViewModel node = new NodeViewModel()
+            {
+                OffsetX = 100,
+                OffsetY = 100,
+                UnitHeight = 100,
+                UnitWidth = 100,
+                Shape = resourceDictionary["Rectangle"],
+                Annotations = new ObservableCollection<IAnnotation>()
+                {
+                    new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(0,0),
+                    },
+                    new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(0.5,0.5),
+                    },
+                    new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(1,1),
+                    },
+                }
+            };
+            (Nodes as NodeCollection).Add(node);
+
+            ConnectorViewModel connector = new ConnectorViewModel()
+            {
+                SourcePoint = new Point(250, 50),
+                TargetPoint = new Point(350, 150),
+                Annotations = new ObservableCollection<IAnnotation>()
+                {
+                    new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(0,0),
+                        Length = 0,
+                    },
+                    new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(0,0),
+                        Length = 0.5,
+                    },
+                     new AnnotationEditorViewModel()
+                    {
+                        Content = "Annotation",
+                        Offset = new Point(0,0),
+                        Length = 1,
+                    },
+                },
+            };
+
+            (Connectors as ConnectorCollection).Add(connector);
+        }
+    }
+}
