02fd7f115a682d0b486d7e9a805958aa6bbd6d83
KarkuvelRajan <63278195+KarkuvelRajan@users.noreply.github.com>
2022-02-17 15:11:55 +0530

    WPF-60148 Sample for Expand/Collapse in MultiParent Layout is added
    WPF-60148 Sample for Expand/Collapse in MultiParent Layout is added

---
+++ Samples/Automatic Layout/Expand Collapse in MultiParent Layout/Expand Collapse in MultiParent Layout/DiagramVM.cs
@@ -0,0 +1,151 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.UI.Xaml.Diagram.Controls;
+using Syncfusion.UI.Xaml.Diagram.Layout;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Expand_Collapse_in_MultiParent_Layout
+{
+    public class MultiParent : DiagramViewModel
+    {
+        public Button prevbutton = null;
+        public MultiParent()
+        {
+            //Initialize Diagram Properties
+            Connectors = new ObservableCollection<ConnectorVM>();
+            Menu = null;
+            Tool = Tool.MultipleSelect;
+            HorizontalRuler = new Ruler { Orientation = Orientation.Horizontal };
+            VerticalRuler = new Ruler { Orientation = Orientation.Vertical };
+            DefaultConnectorType = ConnectorType.Orthogonal;
+
+            // Initialize DataSourceSettings for SfDiagram
+            DataSourceSettings = new DataSourceSettings()
+            {
+                ParentId = "ReportingPerson",
+                Id = "Name",
+                DataSource = GetData(),
+            };
+
+            // Initialize LayoutSettings for SfDiagram
+            LayoutManager = new LayoutManager()
+            {
+                Layout = new DirectedTreeLayout()
+                {
+                    Type = LayoutType.Hierarchical,
+                    Orientation = TreeOrientation.TopToBottom,
+                    AvoidSegmentOverlapping = true,
+                    HorizontalSpacing = 40,
+                    VerticalSpacing = 40,
+                },
+            };
+        }
+
+        #region Private Variables
+
+        private ICommand _Orientation_Command;
+
+        #endregion
+
+        #region Commands
+        public ICommand Orientation_Command
+        {
+            get { return _Orientation_Command; }
+            set
+            {
+                if (_Orientation_Command != value)
+                {
+                    _Orientation_Command = value;
+                    onPropertyChanged("Orientation_Command");
+                }
+            }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void onPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+        /// <summary>
+        /// Method to Get Data for DataSource
+        /// </summary>
+        /// <param name="data"></param>
+        private DataItems GetData()
+        {
+            DataItems data = new DataItems();
+
+            data.Add(new ItemInfo("n11", "#ff6329") { ReportingPerson = new List<string>() });
+
+            data.Add(new ItemInfo("n12", "#ff6329") { ReportingPerson = new List<string>() });
+
+            data.Add(new ItemInfo("n13", "#ff6329") { ReportingPerson = new List<string>() });
+
+            data.Add(new ItemInfo("n21", "#941100") { ReportingPerson = new List<string> { "n11", "n12", "n13" } });
+
+            return data;
+        }
+    }
+
+    public class ConnectorVM : ConnectorViewModel
+    {
+        public ConnectorVM()
+            : base()
+        {
+            this.CornerRadius = 10;
+        }
+
+        private bool _isVisible = true;
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                if (_isVisible == value) return;
+                _isVisible = value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Source {(SourceNode as NodeViewModel).Content} - Target {(TargetNode as NodeViewModel).Content} - Visible {IsVisible}";
+        }
+    }
+
+
+    public class ItemInfo
+    {
+        public ItemInfo(string name, string color)
+        {
+            this.Name = name;
+            this.RatingColor = color;
+        }
+
+        public string RatingColor { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> ReportingPerson { get; set; }
+    }
+
+    public class DataItems : ObservableCollection<ItemInfo>
+    {
+
+    }
+}
---
+++ Samples/Automatic Layout/Expand Collapse in MultiParent Layout/Expand Collapse in MultiParent Layout/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expand_Collapse_in_MultiParent_Layout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ObservableCollection<object> SelectedNodeCollection => (sfdiagram.SelectedItems as SelectorViewModel)?.Nodes as ObservableCollection<object>;
+
+        private ObservableCollection<NodeViewModel> NodeCollection => sfdiagram.Nodes as ObservableCollection<NodeViewModel>;
+
+        public ObservableCollection<ItemInfo> AllCards => sfdiagram.DataSourceSettings.DataSource as ObservableCollection<ItemInfo>;
+
+        private ObservableCollection<ConnectorVM> ConnectorsCollection => sfdiagram.Connectors as ObservableCollection<ConnectorVM>;
+
+        private void ExpandCollapse_Click(object sender, RoutedEventArgs e)
+        {
+            var workingCollection = SelectedNodeCollection;
+            var selectednode = (sfdiagram.SelectedItems as SelectorViewModel).SelectedItem;
+            ItemInfo obj = null;
+            if(selectednode != null && selectednode is NodeViewModel)
+            {
+                obj = (ItemInfo)(selectednode as NodeViewModel).Content;
+            }
+
+            if (obj != null)
+            {
+                workingCollection = new ObservableCollection<object> { NodeCollection.FirstOrDefault(j => j.Content == obj) };
+            }
+            if (workingCollection.Count <= 0) return;
+            foreach (NodeViewModel selectedItem in workingCollection) //Multiple Nodes can be selected and their child node can be collapsed/expanded at the same time
+            {
+                var expandCollapseParameter = new ExpandCollapseParameter
+                {
+                    Node = selectedItem,
+                    IsUpdateLayout = true
+                };
+                var graphInfo = sfdiagram.Info as IGraphInfo;
+                var isExpanded = selectedItem.IsExpanded; // Without this get operation, the IsExpanded property of the node is not updated.
+                graphInfo?.Commands.ExpandCollapse.Execute(expandCollapseParameter);
+
+                var currentCardTree = new List<ItemInfo>();
+                var content = selectedItem.Content as ItemInfo;
+                currentCardTree.AddRange(FindChildren(AllCards.ToList(), content));
+
+                var childNodes = NodeCollection.Where(j => currentCardTree.Contains(j.Content)).ToList();
+                var childConnectors = ConnectorsCollection.Where(j => childNodes.Contains(j.TargetNode)).ToList();
+                childConnectors.ForEach(j => j.IsVisible = !isExpanded);
+            }
+        }
+
+        private List<ItemInfo> FindChildren(List<ItemInfo> source, ItemInfo root)
+        {
+            return source.Where(j => j.ReportingPerson.Contains(root.Name))
+                .Union(source.Where(j => j.ReportingPerson.Contains(root.Name)).SelectMany(j => FindChildren(source, j)))
+                .ToList();
+        }
+    }
+}
