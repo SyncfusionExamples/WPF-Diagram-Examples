bc9d56d5449aa45e6534a68141e37d784d21addf
deepa_thiruppathy <deepa.thiruppathy@syncfusion.com>
2022-05-12 16:59:14 +0530

    WPF-60582  How to bind the SelectedItems property of SfDiagram to ViewModel property
    Added the sample for KB

---
+++ Samples/Selection/BindingSelectedItemViewToViewModel/Sample/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Syncfusion.UI.Xaml.Diagram;
+
+namespace SelectedItems_SfDiagram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    //To Represent the view model class for SfDiagram to bind SelectedItems property of SfDiagram view to ViewModel.
+    public class CustomVM : INotifyPropertyChanged
+    {
+        public CustomVM()
+        {
+        }
+
+        private SelectorViewModel selectedItems = new SelectorViewModel()
+        {
+            Nodes = new ObservableCollection<NodeViewModel>(),
+            Connectors = new ObservableCollection<ConnectorViewModel>(),
+        };
+
+        /// <summary>
+        /// Gets or sets the SelectedItems which is to bind the custom SelectorViewModel to SelectedItems property of SfDiagram view.
+        /// </summary>
+        public SelectorViewModel SelectedItems
+        {
+            get { return selectedItems; }
+            set
+            {
+                if (selectedItems != value)
+                {
+                    selectedItems = value;
+                    OnPropertyChanged("SelectedItems");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this,new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
