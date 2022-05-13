938fbe428552ce1f1f6755c0b97df710dacea735
deepa_thiruppathy <deepa.thiruppathy@syncfusion.com>
2022-05-13 10:49:28 +0530

    WPF-61005 How to add ToolTip for Diagram objects of Node, NodePort in WPF Diagram
    Added sample for KB

---
+++ Samples/ToolTip/ToolTipForNodeAndNodePorts/Sample/ViewModel/CustomNodePortVM.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePortCreation
+{
+    /// <summary>
+    /// Represents a custom class to NodePortViewModel to add custom tooltip properties to it.
+    /// </summary>
+    public class CustomNodePortVM : NodePortViewModel
+    {
+        private string toolTipContent;
+
+        /// <summary>
+        /// Gets or sets tool tip content to the node ports.
+        /// </summary>
+        public string ToolTipContent
+        {
+            get
+            {
+                return toolTipContent;
+            }
+            set
+            {
+                if (toolTipContent != value)
+                {
+                    toolTipContent = value;
+                    OnPropertyChanged("ToolTipContent");
+                }
+            }
+        }
+    }
+}
+
---
+++ Samples/ToolTip/ToolTipForNodeAndNodePorts/Sample/ViewModel/CustomNodeVM.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePortCreation
+{
+    /// <summary>
+    /// Represents a custom class to NodeViewModel to add custom tooltip properties to it.
+    /// </summary>
+    class CustomNodeVM : NodeViewModel
+    {
+        private string toolTipContent;
+
+        /// <summary>
+        /// Gets or sets tool tip content to the node ports.
+        /// </summary>
+        public string ToolTipContent
+        {
+            get
+            {
+                return toolTipContent;
+            }
+            set
+            {
+                if (toolTipContent != value)
+                {
+                    toolTipContent = value;
+                    OnPropertyChanged("ToolTipContent");
+                }
+            }
+        }
+    }
+}
