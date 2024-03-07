bffc5d6baf205d531c60b2611d96fbfa56a4f3a3
KarkuvelRajan <63278195+KarkuvelRajan@users.noreply.github.com>
2024-03-07 11:55:18 +0530

    WPF-874940 Selector sample is added
    WPF-874940 Selector sample is added

---
+++ Samples/Selection/Selector/Selector/View/Diagram.cs
@@ -0,0 +1,19 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GettingStarted_Selector.View
+{    
+    public class Diagram : SfDiagram
+    {       
+        //Apply Selectors
+
+        public Selector SFSelector = new Selector();
+        protected override Selector GetSelectorForItemOverride(object item)
+        {
+            return SFSelector;
+        }
+    }
+}
