73a76a67268e08999628eb75704865e754ac5790
ArunKumarSF4054 <arunkumar.sivakumar@syncfusion.com>
2024-09-01 18:11:40 +0530

    WPF-900304_NewSamplesAdded

---
+++ Samples/Node/SerializeImageNode/SerializeImageNode/ImageAnnotationViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using SyncFusionDiagramTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SyncFusionDiagramTest
+{
+    public class ImageAnnotationViewModel : DiagramViewModel
+    {
+        public ImageAnnotationViewModel()
+        {
+            //Initialize the nodes and connectors collection
+            this.Nodes = new NodeCollection();
+            this.Connectors = new ConnectorCollection();
+
+            //Create the node
+            NodeViewModel node = new NodeViewModel();
+            node.OffsetX = 150;
+            node.OffsetY = 150;
+            node.UnitHeight = 150;
+            node.UnitWidth = 150;
+
+            //Create the annotation collection to the node
+            node.Annotations = new AnnotationCollection()
+            {
+                new AnnotationEditorViewModel()
+                {
+                    //adding image path as annotation content
+                    Content=@"F:\Project\삼성물산\Mindfusion\SyncFusionDiagramTest\EC_Nodes\Images\component-1-fill.png",
+                    VerticalAlignment=VerticalAlignment.Center,
+                    HorizontalAlignment=HorizontalAlignment.Center,
+
+                    //adding ViewTemplate property 
+                    ViewTemplate=App.Current.Resources["viewtemplate"]as DataTemplate,
+                    
+                    //adding EditTemplate property
+                    EditTemplate=App.Current.Resources["edittemplate"] as DataTemplate
+
+                }
+            };
+            node.Shape = new RectangleGeometry() { Rect = new Rect(0, 0, 150, 150) };
+            
+
+
+
+            //adding shape style to the node
+            node.ShapeStyle = App.Current.Resources["nodeshapestyle"] as Style;
+            //Adding the created node into node's collection
+            (this.Nodes as NodeCollection).Add(node);
+        }
+    }
+}
---
+++ Samples/Node/Switch_Icon_visibility/Icon Visibility/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.UI.Xaml.Diagram.Layout;
+using Syncfusion.UI.Xaml.Diagram.Serializer;
+using Syncfusion.UI.Xaml.Diagram.Stencil;
+using Syncfusion.UI.Xaml.Diagram.Theming;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Simple_SfDiagram_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Diagram.Nodes = new NodeCollection();
+            NodeViewModel node = new NodeViewModel()
+            {
+                UnitHeight = 50,
+                UnitWidth = 200,
+                OffsetX = 200,
+                OffsetY = 200,
+                Content = new CustomContent() { ShowLock = Visibility.Visible },
+            };
+            
+            (Diagram.Nodes as NodeCollection).Add(node);
+        }
+
+        private void collapse_Click(object sender, RoutedEventArgs e)
+        {
+            (((Diagram.Nodes as NodeCollection).ElementAt(0) as NodeViewModel).Content as CustomContent).ShowLock = Visibility.Collapsed;
+        }
+    }
+
+    public class CustomContent : INotifyPropertyChanged
+    {
+        private Visibility _showlock = Visibility.Visible;
+
+        public Visibility ShowLock
+        {
+            get 
+            { 
+                return _showlock; 
+            }
+            set 
+            { 
+                _showlock = value;
+                OnPropertyChanged("ShowLock");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
