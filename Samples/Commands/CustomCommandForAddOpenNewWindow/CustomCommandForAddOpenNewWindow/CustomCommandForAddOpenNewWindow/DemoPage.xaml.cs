d2802c3aaa3b31528bb01de06259c1e4102bcd18
ArunKumarSF4054 <124353880+ArunKumarSF4054@users.noreply.github.com>
2024-11-18 16:48:32 +0530

    WPF-921922_CustomCommandSampleAdded

---
+++ Samples/Commands/CustomCommandForAddOpenNewWindow/CustomCommandForAddOpenNewWindow/CustomCommandForAddOpenNewWindow/DemoPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomCommand
+{
+    /// <summary>
+    /// Interaction logic for DemoPage.xaml
+    /// </summary>
+    public partial class DemoPage : Page
+    {
+        public DemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Samples/Commands/CustomCommandForAddOpenNewWindow/CustomCommandForAddOpenNewWindow/CustomCommandForAddOpenNewWindow/ViewModel/DiagramVM.cs
@@ -0,0 +1,56 @@
+﻿//using CustomCommand.Utility;
+using Microsoft.Win32;
+using Syncfusion.UI.Xaml.Diagram;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CustomCommand.ViewModel
+{
+    /// <summary>
+    /// ViewModel class for the Diagram, inheriting from DiagramViewModel.
+    /// This class handles commands and manages selected items.
+    /// </summary>
+    public class DiagramVM : DiagramViewModel
+    {
+        /// <summary>
+        /// Command that opens a new window.
+        /// </summary>
+        public ICommand DemoCommand { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the DiagramVM class.
+        /// Sets up command management and adds a custom command for handling the F2 key gesture.
+        /// </summary>
+        public DiagramVM()
+        {
+            // Initialize selected items collection.
+            SelectedItems = new SelectorViewModel();
+
+            // Initialize custom command.
+            DemoCommand = new Command(OnDemoCommand);
+        }
+
+        /// <summary>
+        /// Method that is executed when the DemoCommand is triggered.
+        /// Opens a new window if connectors are selected in the diagram.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        private void OnDemoCommand(object sender)
+        {
+            // Check if connectors are selected in the diagram.
+            if (SelectedItems is SelectorViewModel && (SelectedItems as SelectorViewModel).Connectors != null && ((SelectedItems as SelectorViewModel).Connectors as IEnumerable<object>).Count() > 0)
+            {
+                // Open a new window.
+                Window1 win = new Window1();
+                win.Show();
+            }
+        }
+    }
+}
